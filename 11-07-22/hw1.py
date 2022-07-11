f5fffd6b9ada39d25e28566022fb109a33aa6e48
Amir-Khalakhoev <onsamiyakk@gmail.com>
2022-07-11 17:01:01 +0300

    11-07-22

--- 11-07-22/hw1.py
+++ 11-07-22/hw1.py
@@ -1,7 +1,11 @@
-s = '0' + '1' * 12 + '2' * 15 + '3' * 17
-while ('01' in s) or ('02' in s) or ('03' in s):
-    s = s.replace('01' , '20', 1)
-    s = s.replace('02' , '120', 1)
-    s = s.replace('03' , '302', 1)
-print(s)
+numbers = []
+for i in range(0, 10):
+    numbers.append(int(input()))
+min_number = numbers[0]
 
+for i in range(0, len(numbers)):
+    if numbers[i] < min_number:
+        min_number = numbers[i]
+
+print()
+print(min_number)
